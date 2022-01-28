a3890057c65148adf8ebd25232f6b5a736cb80e4
Desiree Mendes <desireefrost@live.ca>
2022-01-28 18:47:45 -0500

    simple mad-libs

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 num1 = input("Enter a number ")
 num2 = input("Enter another number ")
 
+#can use int or float to convert to numbers
 result = float(num1) + float(num2)
 print(result)
---
+++ mad-libs.py
@@ -0,0 +1,8 @@
+colour = input("Enter a colour: ")
+noun = input("Enter a plural noun: ")
+celeb = input("Enter a celeb: ")
+
+print("roses are " + colour)
+print(noun + " are blue")
+print("i love " + celeb)
+
