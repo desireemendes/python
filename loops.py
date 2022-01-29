7798a5b8fe79f6be0994a4101e792d3525328859
Desiree Mendes <desireefrost@live.ca>
2022-01-29 11:05:58 -0500

    additional loops syntax

--- loops.py
+++ loops.py
@@ -11,3 +11,37 @@ while i <= 10:
   print(i)
   i += 1
 
+#for loops
+for letter in "Giraffe Academy":
+  print(letter)
+
+friends = ["Jim", "Kerry", "Kevin"]
+for friend in friends:
+  print(friend)
+
+#get length of array
+print(len(friends))
+
+#prints 0-9
+for index in range(10):
+  print(index)
+
+#prints 3-9
+for index in range(3, 10):
+  print(index)
+
+
+for index in range(5):
+  if index == 0:
+    print("1st iteration")
+  else:
+    print("not 1st")
+
+def raise_to_power(base_num, pow_num):
+  result = 1
+  #loop through "pow_num" of times
+  for index in range(pow_num):
+    result = result * base_num
+  return result
+
+print(raise_to_power(3, 4))
