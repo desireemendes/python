f01278c6a492847c5b75c6a24ab8fb78af84457c
Desiree Mendes <desireefrost@live.ca>
2022-01-28 22:15:06 -0500

    improved calculator

--- calculator.py
+++ calculator.py
@@ -1,6 +1,22 @@
-num1 = input("Enter a number ")
-num2 = input("Enter another number ")
+# num1 = input("Enter a number ")
+# num2 = input("Enter another number ")
 
 #can use int or float to convert to numbers
-result = float(num1) + float(num2)
-print(result)
+# result = float(num1) + float(num2)
+# print(result)
+
+
+num1 = float(input("Enter a number: "))
+operator = input("Enter operator: ")
+num2 = float(input("Enter another number: "))
+
+if operator == "+":
+  print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "/":
+  print(num1 / num2)
+elif operator == "*":
+  print(num1 * num2)
+else:
+  print("Invalid operator")
