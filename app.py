c3570b30a21bb7393d117b13c5e52e762231aebd
Desiree Mendes <desireefrost@live.ca>
2022-01-29 13:39:15 -0500

    classes/objects

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 #get info from file, r+ to read and write
-employee_file = open("employee.txt", "r")
-for employee in employee_file.readlines():
-  print(employee)
-employee_file.close()
+# employee_file = open("employee.txt", "r")
+# for employee in employee_file.readlines():
+#   print(employee)
+# employee_file.close()
 
 # "a" to append to file, add text onto it
 # employee_file = open("employee.txt", "a")
 # employee_file.write("\nToby - Human Resources")
+
+#import from student file class student
+from Student import Student
+
+student1 = Student("Jason", "Business", 3.1, False)
+print(student1.name)
